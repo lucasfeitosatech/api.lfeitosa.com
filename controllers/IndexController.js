b26d05f51161e7cd73c2007b82052f69aa3632ff
lucasfeitosa.tech <lucasfeitosa.tech@gmail.com>
2021-02-01 23:52:53 -0300

    [ALT] - Normalize return from mysql

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,15 +1,14 @@
 module.exports = {
   async index(req,res) {
-      res.render('index.jsx');
+      res.send('index.jsx');
   },
   async teste(req,res){
     var sql = 'SELECT * FROM Post';
     db.query(sql, function(err, results){      
       if(results.length != null && results.length >= 1){
-         req.session.userId = results[0].id;
-         req.session.user = results[0];
          console.log(results[0].id);
-         res.send(JSON.parse(results));
+         results = results.map(v => Object.assign({}, v));
+         res.send(results);
       }
       else{
          message = 'Credenciais inválidas';
