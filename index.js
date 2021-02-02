925b8ce06b45692c4c9ba03279a845ce5db686b2
lucasfeitosa.tech <lucasfeitosa.tech@gmail.com>
2021-02-01 23:59:58 -0300

    [ALT] - Change request for only accept https

--- index.js
+++ index.js
@@ -17,3 +17,10 @@ app.use(require('./routes'));
 // parametrizar a porta de escuta
 app.listen((process.env.PORT || port), () => console.log(`\uD83C\uDF0F runing at http://localhost${port}`));
 
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+  if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+      res.redirect(`https://${req.hostname}${req.url}`); //Redireciona pra HTTPS
+  else //Se a requisição já é HTTPS
+      next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+});
+
